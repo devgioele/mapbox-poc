ef29b387712efa42e36016da42f4e854b7bfa7bd
devgioele <gioeledevittidev@gmail.com>
2022-04-19 14:55:07 +0200

    Paint plantations depending on properties

--- src/WebMap.tsx
+++ src/WebMap.tsx
@@ -1,4 +1,4 @@
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { Expression, StyleFunction } from 'mapbox-gl';
 import { useEffect, useRef, useState } from 'react';
 
 type RasterTileset = {
@@ -9,6 +9,11 @@ type RasterTileset = {
 
 type VectorTileset = RasterTileset & {
   layers: string[];
+};
+
+const styles = {
+  it: 'mapbox://styles/kultivas/cl25xsqvu00fm14piw9fcc7by',
+  de: 'mapbox://styles/kultivas/cl25ya7qe002o15tcejx3nznb',
 };
 
 const plantations: VectorTileset = {
@@ -24,10 +29,48 @@ const terrain: RasterTileset = {
   tilesetId: 'mapbox.mapbox-terrain-dem-v1',
 };
 
-const meanTemp: RasterTileset = {
-  id: 'meanTemp',
-  url: 'mapbox://kultivas.5lkpbnjt',
-  tilesetId: 'kultivas.5lkpbnjt',
+const elevation: RasterTileset = {
+  id: 'elevation',
+  url: 'mapbox://kultivas.55eo0pq7',
+  tilesetId: 'kultivas.55eo0pq7',
+};
+
+type Expressions = {
+  [index: string]: string | Expression | StyleFunction;
+};
+
+// Documentation of MapBox expressions:
+// https://docs.mapbox.com/mapbox-gl-js/style-spec/expressions
+const colors: Expressions = {
+  appleVarietyFill: [
+    'match',
+    ['get', 'VarietyNam'],
+    ['GOLDEN', 'GOLDEN DELICIOUS B', 'GOLDEN PARSI DA ROSA', 'GOLDEN REINDERS'],
+    '#ffd128',
+    [
+      'BROOKFIELD GALA BAIGENT',
+      'GALA',
+      'GALA DECARLI - FENDECA',
+      'GALA VENUS FENGAL',
+      'GALA SCHNITZER SCHNIGA',
+      'GALASTAR GALAFAB',
+      'GALAVAL',
+      'BUCKEYE GALA SIMMONS',
+      'GALAXI',
+    ],
+    '#a50518',
+    // Fallback
+    '#c5c6d0',
+  ],
+  elevation: [
+    'rgb',
+    // red is higher when feature.properties.temperature is higher
+    ['get', 'temperature'],
+    // green is always zero
+    0,
+    // blue is higher when feature.properties.temperature is lower
+    ['-', 100, ['get', 'temperature']],
+  ],
 };
 
 const initialLng = 11.3677;
@@ -60,12 +103,41 @@ export default function WebMap({ accessToken }: WebMapProps) {
       console.log('Initializing map');
       map.current = new mapboxgl.Map({
         container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v11',
+        // Style of 'basemap'
+        style: styles.de,
         center: [initialLng, initialLat],
         zoom: initialZoom,
       });
       map.current.on('load', () => {
         if (map.current) {
+          // Add terrain raster tileset
+          map.current.addSource(terrain.id, {
+            type: 'raster-dem',
+            url: terrain.url,
+          });
+          const terrainLayerId = `${terrain.id}-default`;
+          map.current.addLayer({
+            id: terrainLayerId,
+            type: 'hillshade',
+            source: terrain.id,
+            paint: {
+              'hillshade-exaggeration': 0.2,
+            },
+          });
+          // Add elevation raster tileset
+          map.current.addSource(elevation.id, {
+            type: 'raster',
+            url: elevation.url,
+          });
+          const elevationLayerId = `${elevation.id}-default`;
+          map.current.addLayer({
+            id: elevationLayerId,
+            type: 'raster',
+            source: elevation.id,
+            paint: {
+              'raster-opacity': 0.5,
+            },
+          });
           // Add vector tileset
           map.current.addSource(plantations.id, {
             type: 'vector',
@@ -78,7 +150,7 @@ export default function WebMap({ accessToken }: WebMapProps) {
             'source-layer': plantations.layers[0],
             layout: {},
             paint: {
-              'fill-color': '#0080ff',
+              'fill-color': colors.appleVarietyFill,
               'fill-opacity': 0.5,
             },
           });
@@ -93,30 +165,6 @@ export default function WebMap({ accessToken }: WebMapProps) {
               'line-width': 1,
             },
           });
-          // Add terrain raster tileset
-          map.current.addSource(terrain.id, {
-            type: 'raster-dem',
-            url: terrain.url,
-          });
-          const terrainLayerId = `${terrain.id}-default`;
-          map.current.addLayer({
-            id: terrainLayerId,
-            type: 'hillshade',
-            source: terrain.id,
-          });
-          // Add mean temperature raster tileset
-          map.current.addSource(meanTemp.id, {
-            type: 'raster',
-            url: meanTemp.url,
-          });
-          const meanTempLayerId = `${meanTemp.id}-default`;
-          map.current.addLayer({
-            id: meanTempLayerId,
-            type: 'raster',
-            source: meanTemp.id,
-          });
-          // Set paint properties
-          map.current.setPaintProperty(meanTempLayerId, 'raster-opacity', 0.5);
         }
       });
     }
